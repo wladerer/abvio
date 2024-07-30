8f14230de33b2411fc240854e37de022f68355f9
William Laderer <wtladerer@gmail.com>
2024-07-30 15:45:41 -0700

    adding write functions

--- src/abvio/io.py
+++ src/abvio/io.py
@@ -1,4 +1,5 @@
 import logging
+import warnings
 import yaml
 import os
 
@@ -53,6 +54,9 @@ class Input:
     @property
     def structure(self) -> Structure:
         
+        if self.structure_dict is None:
+            raise ValueError(f"No structure dictionary found in input file: keys passed are {self.input_dict.keys()}")
+
         structure_model = structure_model_from_input_dict(self.structure_dict)
         structure = structure_model.structure
 
@@ -60,6 +64,9 @@ class Input:
 
     @property
     def incar(self) -> Incar:
+
+        if self.incar_dict is None:
+            raise ValueError(f"No incar dictionary found in input file: keys passed are {self.input_dict.keys()}")
         
         incar_model = IncarModel(incar_dict=self.incar_dict)
         incar = incar_model.incar(self.structure)
@@ -69,9 +76,52 @@ class Input:
     @property
     def kpoints(self) -> Kpoints:
 
+        if self.kpoints_dict is None:
+            raise ValueError(f"No kpoints dictionary found in input file: keys passed are {self.input_dict.keys()}")
+
         kpoints_model = kpoints_model_from_dictionary(self.kpoints_dict)
-        kpoints = kpoints_model.kpoints
+        kpoints = kpoints_model.kpoints()
         
         return kpoints
+
+    @classmethod
+    def from_file(cls, filepath: Path | str):
+        """Creates an Input object from a file
+
+        Args:
+            filepath: The path to the abvio yaml file
+
+        Returns:
+            Input: The Input object
+        """
+
+        input_dict = load(filepath)
+        return cls(input_dict)
+
+    def write_inputs(self, directory: Path | str):
+        """Writes the input files to the directory
+
+        Args:
+            directory: The directory to write the input files to
+        """
+
+        #write only the files associated with non-None dictionaries
+        if self.structure_dict is not None:
+            poscar = Poscar(self.structure)
+            poscar.write_file(os.path.join(directory, "POSCAR"))
+
+        if self.incar_dict is not None:
+            incar = self.incar
+
+            if incar.check_params():
+                warnings.warn("INCAR file contains unrecognized tags or values", UserWarning)
+
+            incar.write_file(os.path.join(directory, "INCAR"))
+
+        if self.kpoints_dict is not None:
+            kpoints = self.kpoints
+            kpoints.write_file(os.path.join(directory, "KPOINTS"))
+
+
 
 
