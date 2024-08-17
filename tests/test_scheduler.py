0e9c81883b11bb16401432cb8023824a168c989f
William Laderer <wtladerer@gmail.com>
2024-08-17 08:16:08 -0700

    adding scheduler tests

---
+++ tests/test_scheduler.py
@@ -0,0 +1,79 @@
+
+import unittest
+import os
+from pathlib import Path
+from dask_jobqueue.pbs import PBSJob
+from dask_jobqueue.slurm import SLURMJob
+
+import abvio.scheduler as scheduler
+
+base_path = Path(__file__).parent
+files_dir = os.path.join(base_path, "files")
+
+class TestFileCreation(unittest.TestCase):
+    def test_job_type_from_scheduler(self):
+        """Tests if the job_type_from_scheduler function returns the correct job type"""
+        self.assertEqual(scheduler.job_type_from_scheduler("pbs"), PBSJob)
+        self.assertEqual(scheduler.job_type_from_scheduler("slurm"), SLURMJob)
+
+    def test_job_creation(self):
+        """Tests if the Job class can create a job object"""
+        directives = {
+            "queue": "regular",
+            "walltime": "00:30:00",
+            "cores": 3,
+            "memory": "2GB",
+            "job_name": "test_job"
+        }
+
+        pbs_job = scheduler.Job("pbs", directives)
+        slurm_job = scheduler.Job("slurm", directives)
+        self.assertIsInstance(pbs_job.job, PBSJob)
+        self.assertIsInstance(slurm_job.job, SLURMJob)
+
+    def test_write_to_file(self):
+        """Tests if the Job class can write the job script to a file"""
+        directives = {
+            "queue": "regular",
+            "walltime": "00:30:00",
+            "cores": 3,
+            "memory": "2GB",
+            "job_name": "test_job"
+        }
+
+        pbs_job = scheduler.Job("pbs", directives)
+        slurm_job = scheduler.Job("slurm", directives)
+
+        pbs_job.to_file(os.path.join(files_dir, "pbs_job.sh"))
+        slurm_job.to_file(os.path.join(files_dir, "slurm_job.sh"))
+
+        self.assertTrue(os.path.exists(os.path.join(files_dir, "pbs_job.sh")))
+        self.assertTrue(os.path.exists(os.path.join(files_dir, "slurm_job.sh")))
+        
+        os.remove(os.path.join(files_dir, "pbs_job.sh"))
+        os.remove(os.path.join(files_dir, "slurm_job.sh"))
+
+    def test_append_vasp_command(self):
+        """Tests if the Job class can append the VASP command to the job script"""
+        directives = {
+            "queue": "regular",
+            "walltime": "00:30:00",
+            "cores": 3,
+            "memory": "2GB",
+            "job_name": "test_job"
+        }
+
+        pbs_job = scheduler.Job("pbs", directives)
+        slurm_job = scheduler.Job("slurm", directives)
+
+        pbs_job.append("mpirun vasp_std")
+        slurm_job.append("mpirun vasp_std")
+
+        self.assertIn("mpirun vasp_std", str(pbs_job))
+        self.assertIn("mpirun vasp_std", str(slurm_job))
+
+
+
+if __name__ == "__main__":
+    unittest.main()        
+    
